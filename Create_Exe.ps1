e9022be2ad3b0045146df0c9a34209062356c095
Mark B. Rasmussen <56847489+Mark5900@users.noreply.github.com>
2025-01-24 10:36:35 +0100

    Version 1.1.0

--- Create_Exe.ps1
+++ Create_Exe.ps1
@@ -1,12 +1,14 @@
 Import-Module ps2exe
 
+$Version = '1.1.0'
+
 $Spalt = @{
 	inputFile   = '.\Script.ps1'
-	outputFile  = '.\PowerPack Password Encryptor.exe'
+	outputFile  = ".\PowerPack Password Encryptor $Version.exe"
 	title       = 'PowerPack Password Encrypte/Decrypter'
 	description = 'Used to encrypt and decrypt passwords that you use in PowerPack Scripts'
 	company     = 'Mark5900'
-	version     = '1.0.1'
+	version     = $Version
 }
 
 Invoke-ps2exe @Spalt -noConsole -noOutput -noError -noVisualStyles
